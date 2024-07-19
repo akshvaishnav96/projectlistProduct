7788d2e7bb411f21fc017429accc5e4dd4744d54
akshvaishnav96 <vaishnavaksh@gmail.com>
2024-07-19 19:48:11 +0530

    margin added in product name

--- src/app/components/home/PlanName.jsx
+++ src/app/components/home/PlanName.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function PlanName({ planName }) {
-  return <h2 className="text-xl font-semibold">{planName}</h2>;
+  return <h2 className="text-xl font-semibold my-5">{planName}</h2>;
 }
